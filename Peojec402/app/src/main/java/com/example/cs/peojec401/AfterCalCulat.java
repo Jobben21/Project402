6b5b7971262e460e9aee1b283205db9d1e222cc4
Jobben21 <benjarat.de@gmail.com>
2018-04-13 22:12:18 +0700

    menu

--- Peojec402/app/src/main/java/com/example/cs/peojec401/AfterCalCulat.java
+++ Peojec402/app/src/main/java/com/example/cs/peojec401/AfterCalCulat.java
@@ -10,7 +10,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
